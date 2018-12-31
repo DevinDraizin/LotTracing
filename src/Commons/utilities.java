5611f8204a380306f586a8cbd1e437bdc44d5bf5
devindraizin <devindraizin@knights.ucf.edu>
2018-12-30 22:32:41 -0500

    added safe lot number generator

---
+++ src/Commons/utilities.java
@@ -0,0 +1,36 @@
+package Commons;
+
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+import java.util.Random;
+
+public class utilities
+{
+    private static Random rnd = new Random();
+
+    //Copy selected string to user clipboard
+    public static void copyToClip(String msg)
+    {
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+
+        final ClipboardContent content = new ClipboardContent();
+
+        content.putString(msg);
+
+        clipboard.setContent(content);
+    }
+
+    //Generate a unique Component Lot Number
+    //8 digit integer that does not exist in
+    //the Component_Lots table
+    public static int generateUniqueCompLotNumber()
+    {
+        int n;
+
+        do { n = 10000000 + rnd.nextInt(90000000); }while(!DAL.componentDAO.checkComponentLotNum(n));
+
+        return n;
+    }
+
+}
