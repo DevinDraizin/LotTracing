88dba76e1e4732043daaf4129fef13007f474a6d
devindraizin <devindraizin@knights.ucf.edu>
2019-01-10 01:18:19 -0500

    Component Lot # generator now generates strings instead of ints

--- src/Commons/utilities.java
+++ src/Commons/utilities.java
@@ -24,13 +24,13 @@ public class utilities
     //Generate a unique Component Lot Number
     //8 digit integer that does not exist in
     //the Component_Lots table
-    public static int generateUniqueCompLotNumber()
+    public static String generateUniqueCompLotNumber()
     {
         int n;
 
-        do { n = 10000000 + rnd.nextInt(90000000); }while(!DAL.componentDAO.checkComponentLotNum(n));
+        do { n = 10000000 + rnd.nextInt(90000000); }while(!DAL.componentDAO.checkComponentLotNum(String.valueOf(n)));
 
-        return n;
+        return String.valueOf(n);
     }
 
 }
