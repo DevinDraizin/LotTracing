fcd539edcce7962ecda3ea97e0fd72b0dfde6afc
devindraizin <devindraizin@knights.ucf.edu>
2019-02-01 12:46:44 -0500

    Added method for validating custom Assembly Lot Numbers

--- src/DAL/lotNumbersDAO.java
+++ src/DAL/lotNumbersDAO.java
@@ -4,6 +4,7 @@ import LotNumbers.componentLot;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class lotNumbersDAO
@@ -41,6 +42,41 @@ public class lotNumbersDAO
             try { if (stmt != null) stmt.close(); } catch (Exception ignored) {}
         }
 
+        return false;
+    }
+
+    //Returns true if num is unique and false if it is found
+    //in the assembly_lots table
+    public static boolean checkAssemblyLotNum(String num)
+    {
+        PreparedStatement stmt = null;
+        ResultSet myRst = null;
+
+        try
+        {
+            String pstmt = " SELECT COUNT(*) FROM Assembly_Lot_Numbers WHERE Assembly_Lot_Number = ?;";
+
+            stmt = DBConnectionManager.con.prepareStatement(pstmt);
+            stmt.setString(1,num);
+            myRst = stmt.executeQuery();
+
+            myRst.next();
+
+            return myRst.getInt(1) == 0;
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try { if (myRst != null) myRst.close(); } catch (Exception ignored) {}
+            try { if (stmt != null) stmt.close(); } catch (Exception ignored) {}
+        }
+
+        //we should never get here unless the database fails in which case
+        //we default to not allowing creation of new assembly lot numbers to
+        //preserve data integrity
         return false;
     }
 }
