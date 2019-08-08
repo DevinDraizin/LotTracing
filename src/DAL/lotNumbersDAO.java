121077690316d8db4eaa2157a985386a98a6dab2
devindraizin <devindraizin@knights.ucf.edu>
2019-08-07 21:31:51 -0400

    Added data access methods for assembly lots

--- src/DAL/lotNumbersDAO.java
+++ src/DAL/lotNumbersDAO.java
@@ -1,15 +1,20 @@
 package DAL;
 
+import LotNumbers.assemblyLot;
 import LotNumbers.componentLot;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableList;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class lotNumbersDAO
 {
-    //Insert a new purchaseOrder into the database
+    //Insert a new componentLot into the database
     public static boolean insertComponentLot(componentLot lot)
     {
         PreparedStatement stmt = null;
@@ -79,4 +84,133 @@ public class lotNumbersDAO
         //preserve data integrity
         return false;
     }
+
+    //Auxiliary method to getComponentLotList()
+    //this method will take a specific row from the Component_Lot
+    //table and convert the data into a componentLot object.
+    //once converted, we return a componentLot object
+    private static componentLot extractComponentLotFromResultSet(ResultSet myRs) throws SQLException
+    {
+        componentLot componentLot = new componentLot();
+
+        componentLot.ComponentLotNumber = new SimpleStringProperty(myRs.getString(1));
+        componentLot.receiveDate = (myRs.getDate(2).toLocalDate());
+        componentLot.receiveQty = new SimpleIntegerProperty(myRs.getInt(3));
+        componentLot.vendorPO = new SimpleStringProperty(myRs.getString(4));
+        componentLot.vendorID = new SimpleIntegerProperty(myRs.getInt(5));
+        componentLot.componentPartNumber = new SimpleStringProperty(myRs.getString(6));
+
+
+        return componentLot;
+    }
+
+
+    //Here we pull all Component_Lot data from the table and
+    //convert them into an observable list of componentLot
+    //objects.
+    public static void getComponentLotList(ObservableList<componentLot> componentLotList)
+    {
+
+        componentLotList.clear();
+
+        PreparedStatement stmt = null;
+        ResultSet myRst = null;
+
+        try
+        {
+            String pstmt = "SELECT * FROM Component_Lots;";
+
+            stmt = DBConnectionManager.con.prepareStatement(pstmt);
+            myRst = stmt.executeQuery();
+
+            while(myRst.next())
+            {
+                componentLotList.add(extractComponentLotFromResultSet(myRst));
+            }
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try { if (myRst != null) myRst.close(); } catch (Exception ignored) {}
+            try { if (stmt != null) stmt.close(); } catch (Exception ignored) {}
+        }
+
+    }
+
+
+    //This method will take an assembly lot number without the ending unique character and
+    //it will locate all the assembly lot numbers that correspond to lots of the same type
+    //on the same day and store them all in an array.
+    public static ArrayList<String> getSameDayAssemblyLots(String lot)
+    {
+        ArrayList<String> results = new ArrayList<>();
+
+        PreparedStatement stmt = null;
+        ResultSet myRst = null;
+
+        try
+        {
+            String pstmt = "SELECT Assembly_Lot_Number FROM Assembly_Lot_Numbers WHERE Assembly_Lot_Number LIKE ?;";
+
+            stmt = DBConnectionManager.con.prepareStatement(pstmt);
+            stmt.setString(1,lot + "%");
+            myRst = stmt.executeQuery();
+
+            while(myRst.next())
+            {
+                results.add(myRst.getString(1));
+            }
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try { if (myRst != null) myRst.close(); } catch (Exception ignored) {}
+            try { if (stmt != null) stmt.close(); } catch (Exception ignored) {}
+        }
+
+        return results;
+
+    }
+
+    //Insert a new assemblyLot into the database
+    public static boolean insertAssemblyLot(assemblyLot lot)
+    {
+        PreparedStatement stmt = null;
+
+        try
+        {
+            String pstmt = "INSERT INTO Assembly_Lot_Numbers (Assembly_Lot_Number, Assembly_Date, Assembly_Qty, Memo, Part_Number) VALUES (?,?,?,?,?);";
+
+            stmt = DBConnectionManager.con.prepareStatement(pstmt);
+
+            stmt.setString(1,lot.AssemblyLotNumber.get());
+            stmt.setDate(2,Date.valueOf(lot.assembleDate));
+            stmt.setInt(3, lot.assembleQty.get());
+            stmt.setString(4,lot.memos.get());
+            stmt.setString(5,lot.partNumber.get());
+
+
+            stmt.executeUpdate();
+
+            return true;
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try { if (stmt != null) stmt.close(); } catch (Exception ignored) {}
+        }
+
+        return false;
+    }
+
+
 }
