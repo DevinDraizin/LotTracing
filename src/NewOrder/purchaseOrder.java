bedad8f763f9912c8aa43f29dbe2511ec7950312
devindraizin <devindraizin@knights.ucf.edu>
2020-06-19 21:02:13 -0400

    Added functionality to complete purchase orders after all parts have shipped

--- src/NewOrder/purchaseOrder.java
+++ src/NewOrder/purchaseOrder.java
@@ -1,6 +1,7 @@
 package NewOrder;
 
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 import java.time.LocalDate;
@@ -11,17 +12,19 @@ public class purchaseOrder extends RecursiveTreeObject<purchaseOrder>
     public SimpleStringProperty SONumber;
     public LocalDate PODate;
     public LocalDate dueDate;
-    public Integer buyerID;
+    public SimpleIntegerProperty buyerID;
     public SimpleStringProperty memos;
+    public Boolean complete;
 
-    public purchaseOrder(String PONumber, String SONumber, LocalDate PODate, LocalDate dueDate, Integer buyerID, String memos)
+    public purchaseOrder(String PONumber, String SONumber, LocalDate PODate, LocalDate dueDate, Integer buyerID, String memos, boolean complete)
     {
         this.PONumber = new SimpleStringProperty(PONumber);
         this.SONumber = new SimpleStringProperty(SONumber);
         this.PODate = PODate;
         this.dueDate = dueDate;
-        this.buyerID = buyerID;
+        this.buyerID = new SimpleIntegerProperty(buyerID);
         this.memos = new SimpleStringProperty(memos);
+        this.complete = complete;
     }
 
     public purchaseOrder()
@@ -32,5 +35,6 @@ public class purchaseOrder extends RecursiveTreeObject<purchaseOrder>
         this.dueDate = null;
         this.buyerID = null;
         this.memos = null;
+        this.complete = false;
     }
 }
