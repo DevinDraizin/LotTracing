0de2869986ff6977699ed72123cb94f3355e7efd
devindraizin <devindraizin@knights.ucf.edu>
2020-06-30 23:14:07 -0400

    Override hashCode and equals method to use transactionID to maintain object uniqueness

--- src/ShipItems/orderedPartShipment.java
+++ src/ShipItems/orderedPartShipment.java
@@ -19,4 +19,20 @@ public class orderedPartShipment extends RecursiveTreeObject<orderedPartShipment
         this.orderedPart = new orderedPart();
         this.amountToShip = new SimpleIntegerProperty(0);
     }
+
+
+    //Since this object is mutable we will override hashcode and equals
+    //to operate using the transactionID since we know that is always
+    //unique and immutable.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        return this.orderedPart.transactionID.get() == ((orderedPartShipment) o).orderedPart.transactionID.get();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.orderedPart.transactionID.get();
+    }
 }
